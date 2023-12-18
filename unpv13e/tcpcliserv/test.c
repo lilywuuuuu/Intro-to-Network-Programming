4631b7e3679a4fb764735c6fb3a6d5c0c911ca59
hcyu0101 <154235662+hcyu0101@users.noreply.github.com>
2023-12-18 22:50:51 +0800

    Update test.c

--- unpv13e/tcpcliserv/test.c
+++ unpv13e/tcpcliserv/test.c
@@ -1,7 +1,24 @@
-#include <ncurses.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <ncurses.h>
 #include <string.h>
+#include <unistd.h>
 
+void card(){
+    move(3,71);
+    printw("|------|");
+    move(4,71);
+    printw("|      |");
+    move(5,71);
+    printw("|      |");
+    move(6,71);
+    printw("|      |");
+    move(7,71);
+    printw("|      |");
+    move(8,71);
+    printw("|------|");
+    return;
+}
 void line(){
     printw("=============================================================================================\n");
 }
@@ -33,10 +50,24 @@ int main(void) {
         int score[5] = {10, 3, 8, 1};
         int id[5] = {9, 5, 3, 1};
         char name[5][15] = {"Bartholomew", "Lily", "Johnathan", "Mary"};
-
+        WINDOW *cardwin=newwin(7,48,2,35);
+        
         // print scoreboard 
         scoreboard(score, id, name);
+        for(int y=1;y<=34;y=y+3){
+            mvwprintw(cardwin,1,y,"|------|");
+            mvwprintw(cardwin,2,y,"|* * * |");
+            mvwprintw(cardwin,3,y,"|* * * |");
+            mvwprintw(cardwin,4,y,"|* * * |");
+            mvwprintw(cardwin,5,y,"|* * * |");
+            mvwprintw(cardwin,6,y,"|------|");
 
+            wrefresh(cardwin);
+            usleep(80000);
+            wclear(cardwin);
+        }
+        endwin();
+        card();
         // read input 
         ch = getch();
         if (ch == 'q') {
