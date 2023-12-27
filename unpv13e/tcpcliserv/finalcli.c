354c832ff6ba8ff7af8686f24ad3afb5841606e4
Lily Wu <20021212lilywu@gmail.com>
2023-12-27 15:03:11 +0800

    finalcli

--- unpv13e/tcpcliserv/finalcli.c
+++ unpv13e/tcpcliserv/finalcli.c
@@ -6,31 +6,15 @@
 #include <sys/socket.h>
 #include <sys/time.h>
 #include <unistd.h>
-
 #include "unp.h"
 
-void handle_alarm(int sig) {
-    return;
-}
-
-void line() {
-    printw("=====================================================================================\n");
-}
-
-void scoreboard(int score[5], int id[5], char name[5][15]) {
-    line();
-    printw("|---------------------------|\n");
-    printw("|\t Score Board\t    |\n");
-    printw("|\t\t\t    |\n");
-    printw("| Score   Name           ID |\n");
-    for (int i = 0; i < 4; i++) {
-        printw("|  %-2d     %-15s%-2d |\n", score[i], name[i], id[i]);
-    }
-    printw("|\t\t\t    |\n");
-    printw("|---------------------------|\n");
-    printw("\n\n\n\n\n\n\n\n\n\n");
-    line();
-}
+void handle_alarm(int sig); 
+void line();
+void scoreboard(int score[5], int id[5], char name[5][15]);
+void card();
+void draw(int mousex, int mousey, int blank);
+void show_card(int kind, int num);
+void flip_card(WINDOW* cardwin);
 
 int main(int argc, char **argv) {
     int sockfd;
@@ -135,10 +119,14 @@ int main(int argc, char **argv) {
     cbreak();              // Line buffering disabled, pass characters immediately
     noecho();              // Don't echo characters to the screen
     keypad(stdscr, TRUE);  // Enable the keypad for special keys
+    start_color();
+    init_pair(1,COLOR_RED,COLOR_RED);
+    init_pair(2,COLOR_BLACK,COLOR_WHITE);
+    init_pair(3,COLOR_BLACK,COLOR_BLACK);
     // nodelay(stdscr, TRUE);  // Don't wait for input
 
     // int sock_flags = fcntl(sockfd, F_GETFL, 0);
-    int card_num = 0, round = 0;
+    int card_num = 0, pattern = 0, round = 0;
     int score[5] = {0, 0, 0, 0};
     int player_id[5] = {0, 0, 0, 0};
     char name[5][15] = {"", "", "", ""};
@@ -184,6 +172,11 @@ int main(int argc, char **argv) {
         sscanf(recvline, "%d %d", &card_num, &round);
 
         // print card
+        WINDOW *cardwin=newwin(19,48,1,35);
+        attron(COLOR_PAIR(2));
+        flip_card(cardwin);
+        show_card(0,13);
+        attroff(COLOR_PAIR(2));
         gettimeofday(&start, NULL);
 
         // read input
@@ -255,4 +248,281 @@ int main(int argc, char **argv) {
     }
     endwin();
     exit(0);
+}
+
+void handle_alarm(int sig) {
+    return;
+}
+
+void line() {
+    printw("=====================================================================================\n");
+}
+
+void scoreboard(int score[5], int id[5], char name[5][15]) {
+    line();
+    printw("|---------------------------|\n");
+    printw("|\t Score Board\t    |\n");
+    printw("|\t\t\t    |\n");
+    printw("| Score   Name           ID |\n");
+    for (int i = 0; i < 4; i++) {
+        printw("|  %-2d     %-15s%-2d |\n", score[i], name[i], id[i]);
+    }
+    printw("|\t\t\t    |\n");
+    printw("|---------------------------|\n");
+    printw("\n\n\n\n\n\n\n\n\n\n");
+    line();
+}
+
+void card(){
+    move(1,40);
+    printw("|----------------------|");
+    move(2,40);
+    printw("|                      |");
+    move(3,40);
+    printw("|                      |");
+    move(4,40);
+    printw("|                      |");
+    move(5,40);
+    printw("|                      |");
+    move(6,40);
+    printw("|                      |");
+    move(7,40);
+    printw("|                      |");
+    move(8,40);
+    printw("|                      |");
+    move(9,40);
+    printw("|                      |");
+    move(10,40);
+    printw("|                      |");
+    move(11,40);
+    printw("|                      |");
+    move(12,40);
+    printw("|                      |");
+    move(13,40);
+    printw("|                      |");
+    move(14,40);
+    printw("|                      |");
+    move(15,40);
+    printw("|                      |");
+    move(16,40);
+    printw("|                      |");
+    move(17,40);
+    printw("|----------------------|");
+    return;
+}
+void draw(int mousex, int mousey, int blank){
+    move(mousex,mousey);
+    for(int i=0;i<blank;i++){
+        printw(" ");
+    }
+    return;
+}
+void show_card(int kind, int num){
+    card();
+    if(kind==0){
+        attron(COLOR_PAIR(3));
+        draw(12-4,51,2);
+        draw(13-4,48,8);
+        draw(14-4,49,6);
+        draw(15-4,45,14);
+        draw(16-4,43,18);
+        draw(17-4,45,5);
+        draw(13,51,2);
+        draw(13,54,5);
+        draw(18-4,51,2);
+        draw(15,49,6);
+        attroff(COLOR_PAIR(3));
+    } else if(kind==1){
+        attron(COLOR_PAIR(1));
+        draw(8,51,2);
+        draw(9,50,4);
+        draw(10,48,8);
+        draw(11,46,12);
+        draw(12,44,16);
+        draw(13,46,12);
+        draw(14,48,8);
+        draw(15,50,4);
+        draw(16,51,2);
+        attroff(COLOR_PAIR(1));
+    } else if(kind==2){
+        attron(COLOR_PAIR(1));
+        draw(8,44,4);
+        draw(8,56,4);
+        draw(9,43,7);
+        draw(9,54,7);
+        draw(10,42,9);
+        draw(10,53,9);
+        draw(11,43,18);
+        draw(12,45,14);
+        draw(13,46,12);
+        draw(14,47,10);
+        draw(15,49,6);
+        draw(16,51,2);
+        attroff(COLOR_PAIR(1));
+    } else if(kind==3){
+        attron(COLOR_PAIR(3));
+        draw(8,51,2);
+        draw(9,49,6);
+        draw(10,47,10);
+        draw(11,45,14);
+        draw(12,43,18);
+        draw(13,43,6);
+        draw(13,51,2);
+        draw(13,55,6);
+        draw(14,51,2);
+        draw(15,50,4);
+        draw(16,49,6);
+        attroff(COLOR_PAIR(3));
+    }
+    attron(COLOR_PAIR(3));
+    if(num==1){
+        //45 center
+        draw(2,42,7);
+        draw(3,41,2);
+        draw(3,48,2);
+        draw(4,41,9);
+        draw(5,41,2);
+        draw(5,48,2);
+        draw(6,41,2);
+        draw(6,48,2);
+        
+    }else if(num==2){
+        draw(2,41,9);
+        draw(3,48,2);
+        draw(4,41,9);
+        draw(5,41,2);
+        draw(6,41,9);
+    }else if(num==3){
+        draw(2,41,9);
+        draw(3,48,2);
+        draw(4,41,9);
+        draw(5,48,2);
+        draw(6,41,9);
+    }else if(num==4){
+        draw(2,41,2);
+        draw(2,46,2);
+        draw(3,41,2);
+        draw(3,46,2);
+        draw(4,41,9);
+        draw(5,46,2);
+        draw(6,46,2);
+    }else if(num==5){
+        draw(2,41,9);
+        draw(3,41,2);
+        draw(4,41,9);
+        draw(5,48,2);
+        draw(6,41,9);
+    }else if(num==6){
+        draw(2,41,9);
+        draw(3,41,2);
+        draw(4,41,9);
+        draw(5,41,2);
+        draw(5,48,2);
+        draw(6,41,9);
+    }else if(num==7){
+        draw(2,41,9);
+        draw(3,47,2);
+        draw(4,46,2);
+        draw(5,45,2);
+        draw(6,45,2);
+    }else if(num==8){
+        draw(2,42,7);
+        draw(3,41,2);
+        draw(3,48,2);
+        draw(4,42,7);
+        draw(5,41,2);
+        draw(5,48,2);
+        draw(6,42,7);
+    }else if(num==9){
+        draw(2,41,9);
+        draw(3,48,2);
+        draw(3,41,2);
+        draw(4,41,9);
+        draw(5,48,2);
+        draw(6,41,9);
+    }else if(num==10){
+        draw(2,41,2);
+        draw(3,41,2);
+        draw(4,41,2);
+        draw(5,41,2);
+        draw(6,41,2);
+        draw(2,45,7);
+        draw(3,45,2);
+        draw(3,50,2);
+        draw(4,45,2);
+        draw(4,50,2);
+        draw(5,45,2);
+        draw(5,50,2);
+        draw(6,45,7);
+    }else if(num==11){
+        draw(2,41,9);
+        draw(3,46,2);
+        draw(4,46,2);
+        draw(5,41,2);
+        draw(5,46,2);
+        draw(6,42,5);
+    }else if(num==12){
+        draw(2,41,7);
+        draw(3,41,2);
+        draw(3,46,2);
+        draw(4,41,2);
+        draw(4,45,3);
+        draw(5,41,8);
+        draw(6,47,2);
+    }else if(num==0){
+        draw(2,41,2);
+        draw(2,47,2);
+        draw(3,41,2);
+        draw(3,45,3);
+        draw(4,41,5);
+        draw(5,41,2);
+        draw(5,45,3);
+        draw(6,41,2);
+        draw(6,47,2);
+    }
+    attroff(COLOR_PAIR(3));
+    return;
+}
+void flip_card(WINDOW* cardwin){
+    for(int y=1;y<=16;y=y+3){
+        mvwprintw(cardwin,0, y,"|-----------------------|");
+        mvwprintw(cardwin,1, y,"| * * * * * * * * * * * |");
+        mvwprintw(cardwin,2, y,"|* * * * * * * * * * * *|");
+        mvwprintw(cardwin,3, y,"| * * * * * * * * * * * |");
+        mvwprintw(cardwin,4, y,"|* * * * * * * * * * * *|");
+        mvwprintw(cardwin,5, y,"| * * * * * * * * * * * |");
+        mvwprintw(cardwin,6, y,"|* * * * * * * * * * * *|");
+        mvwprintw(cardwin,7, y,"| * * * * * * * * * * * |");
+        mvwprintw(cardwin,8, y,"|* * * * * * * * * * * *|");
+        mvwprintw(cardwin,9, y,"| * * * * * * * * * * * |");
+        mvwprintw(cardwin,10,y,"|* * * * * * * * * * *  |");
+        mvwprintw(cardwin,11,y,"| * * * * * * * * * * * |");
+        mvwprintw(cardwin,12,y,"|* * * * * * * * * * * *|");
+        mvwprintw(cardwin,13,y,"| * * * * * * * * * * * |");
+        mvwprintw(cardwin,14,y,"|* * * * * * * * * * * *|");
+        mvwprintw(cardwin,15,y,"| * * * * * * * * * |");
+        mvwprintw(cardwin,16,y,"|* * * * * * * * *|");
+        mvwprintw(cardwin,17,y,"| * * * * * * * * |");
+        mvwprintw(cardwin,18,y,"|-----------------|");
+        wrefresh(cardwin);
+        usleep(80000);
+        wclear(cardwin);
+    }
+    for(int y=16;y>=1;y=y-3){
+        mvwprintw(cardwin,0,y,"|-----------------|");
+        mvwprintw(cardwin,1,y,"|* * * * * * * * *|");
+        mvwprintw(cardwin,2,y,"| * * * * * * * * |");
+        mvwprintw(cardwin,3,y,"|* * * * * * * * *|");
+        mvwprintw(cardwin,4,y,"| * * * * * * * * |");
+        mvwprintw(cardwin,5,y,"|* * * * * * * * *|");
+        mvwprintw(cardwin,6,y,"| * * * * * * * * |");
+        mvwprintw(cardwin,7,y,"|* * * * * * * * *|");
+        mvwprintw(cardwin,8,y,"| * * * * * * * * |");
+        mvwprintw(cardwin,9,y,"| * * * * * * * * |");
+        mvwprintw(cardwin,10,y,"|-----------------|");
+        wrefresh(cardwin);
+        usleep(20000);
+        wclear(cardwin);
+    }
+    return;
 }
