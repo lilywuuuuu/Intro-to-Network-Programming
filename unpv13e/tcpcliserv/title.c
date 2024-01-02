8ceddcf92aabd260ed2b8522cf6a3a1d218fcce9
hcyu0101 <154235662+hcyu0101@users.noreply.github.com>
2024-01-02 15:58:37 +0800

    Add files via upload
    new title

--- unpv13e/tcpcliserv/title.c
+++ unpv13e/tcpcliserv/title.c
@@ -5,14 +5,41 @@
 #include <unistd.h>
 
 void title(){
-    move(0, 0);
+    move(0, 21);
     printw("  ____   _                    _               _     \n");
+    move(1,21);
     printw(" / ___| | |  __ _  _ __      | |  __ _   ___ | | __ \n");
+    move(2,21);
     printw(" \\___ \\ | | / _` || '_ \\  _  | | / _` | / __|| |/ / \n");
+    move(3,21);
     printw("  ___) || || (_| || |_) || |_| || (_| || (__ |   <  \n");
+    move(4,21);
     printw(" |____/ |_| \\__,_|| .__/  \\___/  \\__,_| \\___||_|\\_\\ \n");
-    printw("                  |_|\n");
+    move(5,21);
+    printw("                  |_|                               \n");
     move(0, 0); 
+}
+void frame(char name[15], int id){
+    move(6,0);
+    char m[30];
+    sprintf(m,"Welcome to SlapJack, %s.",name);
+    int start=(53-strlen(m))/2;
+    move(6,21);
+    printw("|---------------------------------------------------|\n");
+    move(7,21);
+    printw("|                                                   |\n");
+    move(8,21);
+    printw("|                                                   |\n");
+    move(8,start+21);
+    printw("%s",m);;
+    move(8,52+21);
+    printw("|");
+    move(9,21);
+    printw("|                  Your ID is %-2d.                   |\n", id);
+    move(10,21);
+    printw("|                                                   |\n");
+    move(11,21);
+    printw("|---------------------------------------------------|\n");
 }
 
 int main(void) {
@@ -21,13 +48,12 @@ int main(void) {
     noecho();              // Don't echo characters to the screen
     keypad(stdscr, TRUE);  // Enable the keypad for special keys
     start_color();
-    init_pair(1,COLOR_RED,COLOR_RED);
-    init_pair(2,COLOR_BLACK,COLOR_WHITE);
-    init_pair(3,COLOR_BLACK,COLOR_BLACK);
-    
+    char name[15]="Testttttttttttt";
+    int id=1;
     while(1){
         move(0, 0);  // move the cursor to the beginning of the line
         title();
+        frame(name, id);
         refresh();
     }
 
